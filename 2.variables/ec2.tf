a5db3b41020c8ca48572e851b427de631f1e9a00
Murali Elavarthi <muralis2p@gmail.com>
2025-06-04 12:00:35 +0530

    c2

--- 2.variables/ec2.tf
+++ 2.variables/ec2.tf
@@ -1,24 +1,24 @@
 resource "aws_security_group" "sg21" {
- name= "sg21"
- description="expense project sg21"
- tags = var.sg_tags
- ingress{
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+  name        = "sg21"
+  description = "expense project sg21"
+  tags        = var.sg_tags
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = var.cidr_blocks
- } 
- egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = var.cidr_blocks
- }
+  }
 }
 
 resource "aws_instance" "server1" {
-  ami = var.ami
-  vpc_security_group_ids=[aws_security_group.sg21.id]
-  instance_type=var.instance_type
-  tags=var.ec2_tags
+  ami                    = var.ami
+  vpc_security_group_ids = [aws_security_group.sg21.id]
+  instance_type          = var.instance_type
+  tags                   = var.ec2_tags
 }
--- 2.variables/variables.tf
+++ 2.variables/variables.tf
@@ -1,31 +1,31 @@
 variable "ami" {
-  type = string
-  default = "ami-09c813fb71547fc4f"
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
   description = "This is RHEL9 AMI ID"
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
   default = "t3.micro"
 }
 
 variable "ec2_tags" {
-  type= map
+  type = map(any)
   default = {
-    Project = "expense"
-    Component = "backend"
+    Project     = "expense"
+    Component   = "backend"
     Environment = "dev"
-    Name = "expense-backend-dev"
+    Name        = "expense-backend-dev"
   }
 }
 variable "sg_tags" {
-  type = map
+  type = map(any)
   default = {
     Name = "expense-backend-dev"
   }
 }
 
 variable "cidr_blocks" {
-  type = list(string)
-  default = [ "0.0.0.0/0" ]
+  type    = list(string)
+  default = ["0.0.0.0/0"]
 }
